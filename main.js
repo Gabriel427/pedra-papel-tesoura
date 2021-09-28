8a261d0752f1b6f97318cb6646cff14bfc646f9b
Gabriel427 <joao.g.wolochen@hotmail.com>
2021-09-27 21:28:48 -0300

    corrigo erros

--- main.js
+++ main.js
@@ -1,74 +1,65 @@
+let gameChoise = document.getElementsByClassName('gamechoise')
 let playerChoise = 0;
 let cpuChoise = 0;
 let playerScore = 0;
 let cpuScore = 0 ;
 let winner = -1;
+let win = -1;
+
+for(let i = 0; i < gameChoise.length; i++){
+    gameChoise[i].addEventListener('click', function(treco){
+    let tempId = treco.target.parentNode.id;
+    playerChoise = tempId.split('-')[2];
+    startGame(playerChoise);
+    }
+    )
+}
 
 function logic(option){
     playerChoise = option;
     cpuChoise = Math.floor((Math.random() * 3)+1);
-    //1-paper
-    //2-scissors
-    //3-rock
-    if((playerChoise == 1) && (cpuChoise == 1)) {
-        win = 0;
-    }
-    else if((playerChoise == 1) && (cpuChoise == 2)) {
-        win = 2;
-        cpuScore++;
+    
+    if(playerChoise == cpuChoise){
+        return 0;
     }
     else if((playerChoise == 1) && (cpuChoise == 3)) {
-        win = 1;
-        playerScore++;
+        return 1;
     }
     else if((playerChoise == 2) && (cpuChoise == 1)) {
-        win = 1;
-        playerScore++;
-    }
-    else if((playerChoise == 2) && (cpuChoise == 2)) {
-        win = 0;
-    }
-    else if((playerChoise == 2) && (cpuChoise == 3)) {
-        win = 2;
-        cpuScore++;
-    }
-    else if((playerChoise == 3) && (cpuChoise == 1)) {
-        win = 2;
-        cpuScore++;
+        return 1;
     }
     else if((playerChoise == 3) && (cpuChoise == 2)) {
-        win = 1;
-        playerScore++;
+        return 1;
     }
-    else if((playerChoise == 3) && (cpuChoise == 3)) {
-        win = 0;
-    }
-    return win;
+    else
+        return 2;
     }
 
-    function cleanSelected(){
+function cleanSelected(){
     document.getElementById("player-choise-1").classList.remove('selected')
     document.getElementById("player-choise-2").classList.remove('selected')
     document.getElementById("player-choise-3").classList.remove('selected')
     document.getElementById("cpu-choise-1").classList.remove('selected')
     document.getElementById("cpu-choise-2").classList.remove('selected')
     document.getElementById("cpu-choise-3").classList.remove('selected')
-    }
+}
 
-    function insertSelected(){
+function insertSelected(){
     document.getElementById("player-choise-"+ playerChoise).classList.add('selected')
     document.getElementById("cpu-choise-"+ cpuChoise).classList.add('selected')
-    }
+}
 
-    function displayWinner(){
+function displayWinner(){
     if (winner == 0) {
         document.getElementById("messages").innerHTML = 'Draw'
     }
     else if (winner == 1) {
+        playerScore++;
         document.getElementById("messages").innerHTML = 'Gabriel Win'
         document.getElementById("player-score").innerHTML = playerScore;
     }
     else if (winner == 2) {
+        cpuScore++;
         document.getElementById("messages").innerHTML = 'Cpu Win'
         document.getElementById("cpu-score").innerHTML = cpuScore;
     }
@@ -79,5 +70,4 @@ function startGame(option){
     cleanSelected();
     insertSelected();
     displayWinner(winner);
-
 }
